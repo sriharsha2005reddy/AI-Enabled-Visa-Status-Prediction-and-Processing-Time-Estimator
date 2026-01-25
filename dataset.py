254b2fe10b62e63946ae2cfeb60052b7be850687
sriharsha2005reddy <sriharsha2005singireddy@gmail.com>
2026-01-25 10:23:15 +0530

    Updated new task in the file

--- dataset.py
+++ dataset.py
@@ -1,4 +1,5 @@
 
+#MILESTONE-1
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
@@ -67,6 +68,8 @@ print(df.isnull().sum())
 df.to_csv("clean_visa_dataset.csv", index=False)
 
 
+
+#MILESTONE-2
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -117,5 +120,113 @@ df['fast_processing'] = df['processing_time'].apply(
 
 print("\nDataset after Feature Engineering:")
 print(df.info())
+df.to_csv("milestone2.csv", index=False)
 
 
+#MILESTONE-3
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+
+#loading preprocessed dataset
+df = pd.read_csv("milestone2.csv")
+print("Dataset Loaded Successfully\n")
+print(df.head())
+
+#select features and target
+X = df.drop("processing_time", axis=1)
+y = df["processing_time"]
+
+#train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+#linear regression train and evaluation
+lr = LinearRegression()
+lr.fit(X_train, y_train)
+lr_pred = lr.predict(X_test)
+
+lr_mae = mean_absolute_error(y_test, lr_pred)
+lr_rmse = np.sqrt(mean_squared_error(y_test, lr_pred))
+lr_r2 = r2_score(y_test, lr_pred)
+
+print("\n===== Linear Regression Results =====")
+print("MAE :", lr_mae)
+print("RMSE:", lr_rmse)
+print("R²  :", lr_r2)
+
+
+#random forest train and evaluation
+rf = RandomForestRegressor(random_state=42)
+rf.fit(X_train, y_train)
+rf_pred = rf.predict(X_test)
+
+rf_mae = mean_absolute_error(y_test, rf_pred)
+rf_rmse = np.sqrt(mean_squared_error(y_test, rf_pred))
+rf_r2 = r2_score(y_test, rf_pred)
+
+print("\n===== Random Forest Results =====")
+print("MAE :", rf_mae)
+print("RMSE:", rf_rmse)
+print("R²  :", rf_r2)
+
+#gradient boosting train and evaluation
+gbr = GradientBoostingRegressor(random_state=42)
+gbr.fit(X_train, y_train)
+gbr_pred = gbr.predict(X_test)
+
+gbr_mae = mean_absolute_error(y_test, gbr_pred)
+gbr_rmse = np.sqrt(mean_squared_error(y_test, gbr_pred))
+gbr_r2 = r2_score(y_test, gbr_pred)
+
+print("\n===== Gradient Boosting Results =====")
+print("MAE :", gbr_mae)
+print("RMSE:", gbr_rmse)
+print("R²  :", gbr_r2)
+
+#best model
+model_errors = {
+    "Linear Regression": lr_rmse,
+    "Random Forest": rf_rmse,
+    "Gradient Boosting": gbr_rmse
+}
+
+best_model_name = min(model_errors, key=model_errors.get)
+print("\n===== Best Model Identified =====")
+print("Best Model =", best_model_name)
+if best_model_name == "Linear Regression":
+    best_model = lr
+elif best_model_name == "Random Forest":
+    best_model = rf
+else:
+    best_model = gbr
+
+#final predictions
+final_pred = best_model.predict(X_test)
+
+#final model evaluation
+final_mae = mean_absolute_error(y_test, final_pred)
+final_rmse = np.sqrt(mean_squared_error(y_test, final_pred))
+final_r2 = r2_score(y_test, final_pred)
+
+print("\n===== FINAL MODEL PERFORMANCE =====")
+print("Final MAE :", final_mae)
+print("Final RMSE:", final_rmse)
+print("Final R²  :", final_r2)
+
+#saving the best model
+output = pd.DataFrame({
+    "Actual Processing Time": y_test,
+    "Predicted Processing Time": final_pred
+})
+
+output.to_csv("milestone3_predictions.csv", index=False)
+
+print("\nMilestone 3 Completed Successfully!")
+print("Prediction file saved as milestone3_predictions.csv")
